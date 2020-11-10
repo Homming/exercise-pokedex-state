7b7fee08b883f84796f2a61d746891d771f2001a
Nicholas Torres J. Vasconcelos <nicolastorresjatoba@gmail.com>
2020-11-10 20:18:26 -0300

    Exibe um pokemon por vez, e criado evento de botão para passar elemento da lista de pokemons

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -2,10 +2,33 @@ import React from 'react';
 import Pokemon from './Pokemon';
 
 class Pokedex extends React.Component {
+    constructor() {
+        super();
+        this.pokemonClickEvent = this.pokemonClickEvent.bind(this);
+
+        this.state = {
+            pokemonIndex: 0
+        }
+    }
+
+    pokemonClickEvent() {
+        if(this.state.pokemonIndex < this.props.pokemons.length - 1){
+            this.setState((actualState, _props) => ({
+                pokemonIndex: actualState.pokemonIndex + 1
+            }));
+        } else {
+            this.setState({
+                pokemonIndex: 0
+            });
+        }
+    }
+
     render() {
         return (
             <div className="pokedex">
-                {this.props.pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
+                {this.props.pokemons.filter((_pokemon, index) => this.state.pokemonIndex === index)
+                    .map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
+                <button onClick={this.pokemonClickEvent}>Próximo Pokemon</button>
             </div>
         );
     }
